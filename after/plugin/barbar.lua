b57771db126bc658d6c587ecfda1dbcf7cbd905e
MaximMGG <maxhrunenko@gmail.com>
2025-05-27 19:55:52 +0000

    stable

---
+++ after/plugin/barbar.lua
@@ -0,0 +1,70 @@
+require('barbar').setup {
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+}
+
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<leader>bp', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader>bn', '<Cmd>BufferNext<CR>', opts)
+--vim.keymap.set("n", "<leader>bn", vim.cmd.BufferNext)
+
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- Goto buffer in position...
+map('n', '<leader>b1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<leader>b2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<leader>b3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<leader>b4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<leader>b5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<leader>b6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<leader>b7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<leader>b8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<leader>b9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<leader>b0', '<Cmd>BufferLast<CR>', opts)
+
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+
+-- Goto pinned/unpinned buffer
+--                 :BufferGotoPinned
+--                 :BufferGotoUnpinned
+
+-- Close buffer
+map('n', '<leader>bc', '<Cmd>BufferClose<CR>', opts)
+
+-- Wipeout buffer
+--                 :BufferWipeout
+
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+
+-- Magic buffer-picking mode
+map('n', '<C-p>',   '<Cmd>BufferPick<CR>', opts)
+map('n', '<C-s-p>', '<Cmd>BufferPickDelete<CR>', opts)
+
+-- Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
