36c68a8980cf100be32c401f9721b4aaf42daaff
MaximMGG <maxhrunenko@gmail.com>
2025-07-25 18:18:00 +0000

    init.lua

--- lua/maxim/init.lua
+++ lua/maxim/init.lua
@@ -2,11 +2,14 @@ require("maxim.config")
 require("maxim.remap")
 
 
-vim.cmd.colorscheme("base16-atelier-savanna")
+vim.cmd.colorscheme("base16-terracotta-dark")
 
--- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
--- vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
--- vim.api.nvim_set_hl(0, "EndOfBuffer", {bg = "none"})
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
+vim.api.nvim_set_hl(0, "EndOfBuffer", {bg = "none"})
+vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"})
+vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
+vim.api.nvim_set_hl(0, "CursoreLineNr", {bg = "none"})
 
 
 -- base16-3024
