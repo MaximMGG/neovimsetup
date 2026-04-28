b4c930c6705e3172abb6b42bff9dccf681ff5026
MaximMGG <maxhrunenko@gmail.com>
2026-04-28 20:44:27 +0300

    lazy nvim 0.12 config install

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,11 +14,52 @@ return {
   },
 
   {
-    'mrcjkb/rustaceanvim',
-    version = 5,
-    lazy = false,
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_termitaned.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dupui_config = function()
+        dapui.close()
+      end
 
+    end
   },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+      require("dapui").setup()
+    end
+  },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^9',
+    lazy = false,
+    config = function()
+      local mason_registry = require('mason-registry')
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+      local cfg = require('rustaceanvim.config')
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+        },
+      }
+    end
+  }
+
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
